fdea0d154a2d9ed23c6e911e0ee74c5ed29b8144
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-12-04 20:38:04 +0530

    Add files via upload

---
+++ Area-of-Rectangle/Area-of-Rectangle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length: ");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");
+            int width = int.Parse(Console.ReadLine());
+            int area = length * width;
+            Console.WriteLine("Area of Rectangle is: " + area);
+            Console.ReadKey();
+        }
+    }
+}
