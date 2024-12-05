4262326fb728dabde1ce0f4dade7a9b365aa1f9b
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-12-05 12:10:20 +0530

    Add files via upload

---
+++ 04-12-2024/Hours-Minutes/Hours-Minutes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+ namespace HoursMinutes
+{
+    class HoursMinutes
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter time (in minutes): ");
+            int time = int.Parse(Console.ReadLine());
+            int hour = time / 60;
+            int minute = time % 60;
+            Console.WriteLine("Time: " + hour + " hour(s), " + minute + " minute(s)");
+            Console.ReadKey();
+        }
+    }
+}
