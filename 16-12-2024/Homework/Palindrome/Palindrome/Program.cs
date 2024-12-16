3185d6b2b195590342d9dbfd80516900c398b3f5
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-12-16 22:17:00 +0530

    Add files via upload

---
+++ 16-12-2024/Homework/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            int reverse = 0;
+            int temporary = num;
+            while(temporary != 0)
+            {
+                reverse = (reverse * 10) + (temporary % 10);
+                temporary = temporary / 10;
+            }
+            Console.WriteLine($"Reverse: {reverse}");
+            if(reverse == num)
+            {
+                Console.WriteLine($"{reverse} is Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{reverse} is not Palindrome.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
