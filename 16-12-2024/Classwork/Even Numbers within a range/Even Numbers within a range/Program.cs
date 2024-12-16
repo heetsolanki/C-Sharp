56a92a8d7befe2dcc3f8a39a972a4501871b76e4
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-12-16 21:48:50 +0530

    Add files via upload

---
+++ 16-12-2024/Classwork/Even Numbers within a range/Even Numbers within a range/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    class Even
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter starting number: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("Enter ending number: ");
+            int end = int.Parse(Console.ReadLine());
+            for(int i = start; i <= end; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.Write($"{i} \t");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
