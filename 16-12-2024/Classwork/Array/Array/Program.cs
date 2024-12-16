1aad883f01868219f8f2ee3a766f68124bf63a83
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-12-16 21:54:38 +0530

    Add files via upload

---
+++ 16-12-2024/Classwork/Array/Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter elements: ");
+            int[] a = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write($"{i + 1}: ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Array:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{a[i]}\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
