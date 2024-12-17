0fa50f93b88c324ece2e35ff5cc669894d5e5195
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-12-17 14:13:57 +0530

    Add files via upload

---
+++ 17-12-2024/Classwork/Max using Functions/Max using Functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Max
+{
+    class Max
+    {
+        public int max(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Max m = new Max();
+            Console.WriteLine($"Max: {m.max(a,b)}");
+            Console.ReadKey();
+        }
+    }
+}
