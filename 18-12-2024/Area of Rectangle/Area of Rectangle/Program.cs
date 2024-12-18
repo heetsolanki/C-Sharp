8f3a017d0ca2a4b101f2054b80068379aa342985
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-12-18 20:56:44 +0530

    Add files via upload

---
+++ 18-12-2024/Area of Rectangle/Area of Rectangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Reactangle
+{
+    class Rectangle
+    {
+        public static void Area(int length, int breadth, out int area, out int perimeter) // static method name 'Area'
+        {
+            area = length * breadth;
+            perimeter = 2 * (length + breadth);
+        }
+        static void Main(string[] args)
+        {
+            int area, perimeter;
+            Console.Write("Enter length: ");
+            int length = int.Parse(Console.ReadLine()); // accepting length
+            Console.Write("Enter breadth: "); 
+            int breadth= int.Parse(Console.ReadLine()); // accepting breadth
+            Area(length, breadth, out area, out perimeter); // calling the method, no object required as it a static methods
+            Console.WriteLine($"Area of Rectangle: {area}");
+            Console.WriteLine($"Perimeter of Rectangle: {perimeter}");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ 18-12-2024/Method Overloading/Method Overloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Method
+{
+    class Method
+    {
+        public void addition(int a, int b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public void addition(float a, float b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public void addition(String a, String b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        static void Main(string[] args)
+        {
+            Method m = new Method();
+            m.addition(3, 9);
+            m.addition(3.5f, 53.4f);
+            m.addition("hello", " world");
+            Console.ReadKey();
+        }
+    }
+}
